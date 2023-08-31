c61247e3d0049240b0ad7ade3754bbc37fb71769
zhenxinma <kaola.go@qq.com>
2023-09-01 01:22:54 +0800

    feat:XMUser增加密码

--- pkg/database/xmuser/xmuser.go
+++ pkg/database/xmuser/xmuser.go
@@ -4,12 +4,13 @@ import "time"
 
 // XMUser xm user.
 type XMUser struct {
-	UserAccount string    `json:"user_account" gorm:"column:user_account"`
-	UserID      string    `json:"user_id" gorm:"column:user_id"`
-	UserName    string    `json:"user_name" gorm:"column:user_name"`
-	UserIcon    string    `json:"user_icon" gorm:"column:user_icon"`
-	CreateTime  time.Time `json:"create_time" gorm:"column:create_time"`
-	LastLogin   time.Time `json:"last_login" gorm:"column:last_login"`
+	UserAccount  string    `json:"user_account" gorm:"column:user_account"`
+	UserID       string    `json:"user_id" gorm:"column:user_id"`
+	UserName     string    `json:"user_name" gorm:"column:user_name"`
+	UserPassword string    `json:"user_password" gorm:"column:user_password"`
+	UserIcon     string    `json:"user_icon" gorm:"column:user_icon"`
+	CreateTime   time.Time `json:"create_time" gorm:"column:create_time"`
+	LastLogin    time.Time `json:"last_login" gorm:"column:last_login"`
 }
 
 // New a xm user instance.
