be820138e3108d80e9c92138a6650f9907fec74d
zhenxinma <kaola.go@qq.com>
2023-10-09 01:05:38 +0800

    feat:增加Cros MiddleWare

---
+++ pkg/apphelper/ginhelper/cros_middleware.go
@@ -0,0 +1,19 @@
+package ginhelper
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// Cors Cross Origin Resource Sharing is an HTTP-header based mechanism that allows a server to indicate any origins
+// other than its own from a browser should permit loading resources.
+func Cors() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "PUT", "POST", "DELETE"},
+		AllowHeaders:    []string{"Content-Type", "Origin"},
+		MaxAge:          time.Hour * 24 * 7, // cros 缓存7天,前提浏览器开启缓存.
+	})
+}
