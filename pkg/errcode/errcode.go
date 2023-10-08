b1c95881fc9f3dba21f9f1ad823c995a1f0e890e
zhenxinma <kaola.go@qq.com>
2023-10-08 23:18:24 +0800

    feat:新增ErrorCode

--- pkg/errcode/errcode.go
+++ pkg/errcode/errcode.go
@@ -22,8 +22,17 @@ var (
 	// ErrorParam 请求参数错误.
 	ErrorParam = &CommonResult{Code: 10010, Msg: "请求参数错误"}
 
-	// ErrorGetIndexArticleList 获取文章错误.
-	ErrorGetIndexArticleList = &CommonResult{Code: 10010, Msg: "获取文章列表出错"}
+	// ErrorXMUserTokenExpired 登录态失效
+	ErrorXMUserTokenExpired = &CommonResult{Code: 10011, Msg: "登录态失效"}
+
+	// ErrorSystemError 系统错误
+	ErrorSystemError = &CommonResult{Code: 10012, Msg: "系统错误"}
+
+	// ErrorGORPCError GORPC error
+	ErrorGORPCError = &CommonResult{Code: 10013, Msg: "GORPC Request Error"}
+
+	// ErrorParseJSONError 解析Json失败
+	ErrorParseJSONError = &CommonResult{Code: 10014, Msg: "解析Json失败"}
 
 	// ErrorUserLoginFail 用户登录失败.
 	ErrorUserLoginFail = &CommonResult{Code: 20000, Msg: "用户登录失败,请重试！"}
@@ -33,4 +42,13 @@ var (
 
 	// ErrorUserAccountIllegal 用户账号或密码不合法.
 	ErrorUserAccountIllegal = &CommonResult{Code: 20002, Msg: "用户账号或密码不合法,请重新尝试！"}
+
+	// ErrorUserAccountHaveAlreadyRegistered 用户账号已经注册
+	ErrorUserAccountHaveAlreadyRegistered = &CommonResult{Code: 20003, Msg: "用户已经注册，请稍后重试"}
+
+	// ErrorGetIndexArticleList 获取文章错误.
+	ErrorGetIndexArticleList = &CommonResult{Code: 30010, Msg: "获取文章列表出错"}
+
+	// ErrorCreateArticleError 文章评论失败
+	ErrorCreateArticleError = &CommonResult{Code: 30011, Msg: "评论失败，请稍后重试"}
 )
