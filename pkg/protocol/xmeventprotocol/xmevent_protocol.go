a62574e290c24f5d490c1f73531551e857cbd25f
zhenxinma <kaola.go@qq.com>
2023-10-10 23:21:58 +0800

    feat:增加XMEventKey

---
+++ pkg/protocol/xmeventprotocol/xmevent_protocol.go
@@ -0,0 +1,12 @@
+package xmeventprotocol
+
+// XMEventKey  事件广播Key类型
+type XMEventKey string
+
+// 事件广播Key
+const (
+	// XMEventKeyOfArticleUpdate 文章更新事件
+	XMEventKeyOfArticleUpdate XMEventKey = "xm_event_article_update"
+	// XMEventKeyOfArticleCommentUpdate 文章评论更新事件
+	XMEventKeyOfArticleCommentUpdate XMEventKey = "xm_event_article_comment_update"
+)
