dfdcb89baea598289632c8e9e51f86d7ca5d76ae
zhenxinma <kaola.go@qq.com>
2023-10-13 23:17:11 +0800

    feat:增加归档结构体

---
+++ pkg/database/xmarchive/xmarchive.go
@@ -0,0 +1,14 @@
+package xmarchive
+
+// XMBlogsArchive xm blogs archive
+type XMBlogsArchive struct {
+	ID      int    `json:"id" gorm:"column:id"`
+	Name    string `json:"name" gorm:"column:name"`
+	BKStyle string `json:"bk_style" gorm:"column:bk_style"`
+	Status  int8   `json:"status" gorm:"column:status"`
+}
+
+// New return an archive instance
+func New() *XMBlogsArchive {
+	return &XMBlogsArchive{}
+}
