911fd5e9ee2bfefc57642b7e3f947ce605b8441c
zhenxinma <2280480546@qq.com>
2023-10-22 19:32:25 +0800

    feat:更新Article

--- pkg/database/xmarticle/article.go
+++ pkg/database/xmarticle/article.go
@@ -3,6 +3,9 @@ package xmarticle
 // Article 文章.
 type Article struct {
 	ID        int    `json:"id" gorm:"column:id"`
+	TypeID    int    `json:"type_id" gorm:"column:type_id"` // TypeID 分类ID.
+	Publish   int    `json:"publish" gorm:"column:publish"` // Publish 是否发布.
+	ReadCount int    `json:"read_count" gorm:"column:read_count"`
 	Title     string `json:"title" gorm:"column:title"`
 	Time      string `json:"time" gorm:"column:time"`
 	Author    string `json:"author" gorm:"column:author"`
@@ -10,9 +13,6 @@ type Article struct {
 	SubHead   string `json:"sub_head" gorm:"column:subhead"`
 	Img       string `json:"img" gorm:"column:img"`
 	Type      string `json:"type" gorm:"column:type"`
-	TypeID    int    `json:"type_id" gorm:"column:type_id"` // TypeID 分类ID.
-	Publish   int    `json:"publish" gorm:"column:publish"` // Publish 是否发布.
-	ReadCount int    `json:"read_count" gorm:"column:read_count"`
 }
 
 // New return a article instance
