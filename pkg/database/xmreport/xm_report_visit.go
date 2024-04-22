da76fa09553c27f7d37533912ef4f3e7d507b0c1
zhenxinma <2280480546@qq.com>
2024-04-22 21:45:07 +0800

    feat:增加visit_data结构体

--- pkg/database/xmreport/xm_report_visit.go
+++ pkg/database/xmreport/xm_report_visit.go
@@ -9,3 +9,9 @@ type XMReportVisit struct {
 	IP        string    `json:"ip" gorm:"column:ip"`
 	VisitTime time.Time `json:"visit_time" gorm:"column:visit_time"`
 }
+
+// VisitData 聚合分组统计结果，按照天数。
+type VisitData struct {
+	Data  string `json:"data" gorm:"column:data"`
+	Count int    `json:"count" gorm:"column:count"`
+}
